6213acd7f4fc6658aa4c1d477268e3463eff3873
vintage <athousandpercent1000@gmail.com>
2026-04-13 12:11:31 +0300

    Debug: log DATABASE_URL prefix and length on startup

--- Trippplel/server/src/config/db.js
+++ Trippplel/server/src/config/db.js
@@ -1,6 +1,9 @@
 const prisma = require("../lib/prisma");
 
 const connectDB = async () => {
+  const url = process.env.DATABASE_URL || "";
+  console.log("DATABASE_URL prefix:", url.substring(0, 30));
+  console.log("DATABASE_URL length:", url.length);
   try {
     await prisma.$connect();
     console.log("Supabase (PostgreSQL) connected");
